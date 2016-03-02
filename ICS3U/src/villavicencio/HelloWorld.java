8036a18d250dddc5745f8168295ce9ce839a54c9
Paolavella <paola.villavicencio@student.tdsb.on.ca>
2016-03-02 15:05:10 -0500

    I made a program that prints Hello World to the program.

---
+++ ICS3U/src/villavicencio/HelloWorld.java
@@ -0,0 +1,14 @@
+package villavicencio;
+/* helloWorld.java
+ * this program prints Hello World to the console
+ * Paola Villavicencio
+ * Mar 3 2016
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Hello World");
+	}
+
+}

0884214447e995d6d78511145ef440e0ab74df35
343137220 <343137220@C-3ZYBYR1.tdsb.on.ca>
2016-04-28 15:06:16 -0400

    Meant to choose a random colour to guess the colour of the user

---
+++ ICS3U/src/villavicencio/GuessArray.java
@@ -0,0 +1,53 @@
+package villavicencio;
+
+import java.util.Scanner;
+/* GuessArray.java
+public class GuessArray 
+Meant to choose a random colour to guess the colour of the user
+Paola Villavicencio
+Apr 24 2016
+*/{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner (System.in);		
+	
+        String yesNo;
+        boolean colour = false;
+		
+		
+		
+		String [] guess = new String [] {"blue", "green", "teal", "yellow", "orange", "brown", "red", "charcoal", "pink", "violet", "aqua", "avocado", "gold", "celeste", "cintron", "grey", "silver", "copper", "cyan", "tangerine"};
+		
+		
+		System.out.println("Choose an colour, I will guess which colour you are thing king of! READY?! Click enter.");
+	    int answer = scn.nextInt();
+	    
+	    while (colour == false){
+		
+	    	int i =0;
+	    	int num = (int)(Math.random()* 20);
+	    	System.out.println("Are you thinking on " + guess[i] + "?");
+	    	yesNo = scn.nextLine();
+	    	
+		if (yesNo.equals("y")){
+		System.out.println("I win!");
+			
+		colour = true;	
+		}
+		
+		else if (yesNo.equals("n")){
+		colour = false;
+		
+		}
+		
+	    }
+		
+		
+
+		
+		
+
+	}
+
+}
